eda93d1ddfed8d7ad5725dae436026112ce57bfb
caberos <cabermaldo010788@gmail.com>
2020-07-10 17:06:42 -0400

    task 1 send files

--- src/main/java/com/jalasoft/project/controller/HelloController.java
+++ src/main/java/com/jalasoft/project/controller/HelloController.java
@@ -1,24 +1,34 @@
 package com.jalasoft.project.controller;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 
 /**
  * @author HP
  * @version 1.1
  */
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
 
     @PostMapping
+    @RequestMapping("/hello")
     public String sayHello(@RequestParam(value = "name") String name,
                            @RequestParam(value = "lastName") String lastName) {
         return "Hello " + name + " " + lastName;
     }
 
     @PostMapping
-    public Person sayHelloFile(@RequestBody Person person){
-        return person;
+    @RequestMapping("/helloPerson")
+    public String sayHello(@RequestBody Person person) {
+        return "Hello " + person.name + " " + person.lastName;
     }
+
+    @PostMapping
+    @RequestMapping("/hellowithFile")
+    public String sayHello(@RequestParam(value = "file") MultipartFile file) {
+        return "Hello " +file.getSize();
+    }
+
 
 }
